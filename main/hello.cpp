311f4fbcd5992c8f79ae795f64217cf77c92ab5b
Jeff JIju <jeffjiju122@gmail.com>
2023-02-19 23:38:12 +0530

    Hello.cpp second commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, PESU from PES2UG20CS146\n";
+  cout << "Hello, PESU from PES2UG20CS146,Jeff Jiju\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
   return 0;
